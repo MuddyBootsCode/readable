cb840758df45aab14facd1fa966eb4c442b13e80
MuddyBootsCode <firsttogo98@gmail.com>
2018-02-28 06:53:31 -0600

    refactor: working on bug not allowing deployment on certain browsers

--- src/store.js
+++ src/store.js
@@ -6,7 +6,7 @@ import thunk from 'redux-thunk'
 const store = createStore(
     reducer,
     compose(applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__())
+        window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__()||compose)
 
 )
 
